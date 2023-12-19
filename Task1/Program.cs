502519534cc7be46cae08cdda6c8deac59a4ba8a
Akmal Narziev <146104679+Akmaljon2008@users.noreply.github.com>
2023-12-19 15:04:26 +0300

    fc

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Person person1 = new Person();
+Console.Write("Enter your FirstName : ");
+person1.FirstName = Console.ReadLine();
+Console.Write("Enter your LastName : ");
+person1.LastName = Console.ReadLine();
+Console.Write("Enter your Age : ");
+person1.Age = int.Parse(Console.ReadLine());
+
+System.Console.Write(person1.Getinfo());
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+     
+    public string Getinfo()
+    {
+         return $"FirstName : {this.FirstName},\nLastName : {this.LastName},\nAge : {this.Age}";
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿ 
+Post post1 = new Post();
+post1.Coments = "0";
+post1.like = 10;
+System.Console.WriteLine(post1.about());
+Console.WriteLine("Your command : ");
+Console.WriteLine("(Commands : like; comment )");
+string command = Console.ReadLine();
+Console.WriteLine(post1.setting(command));
+
+
+
+class Post{
+    public string Coments;
+    public int like;
+    public string text;
+
+    public string about()
+    {
+        return $"Comments {this.Coments} Likes {this.like}";
+    }
+    public string setting(string command)
+    {
+        
+        if( command == "like")like++;
+       else if (command == "comment"){
+        Coments = Console.ReadLine();
+        return $"Your coment : {Coments}";
+       }
+       return $"Comments {this.Coments} Likes {this.like}";
+    }
+}
